6c0f97fa2a9d2af455119a913b8987046b6e419f
Oscarmbb <oscar2mbb@gmail.com>
2021-08-21 23:51:54 -0500

    homework 00 finished

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,11 +1,20 @@
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var sumatoria = 0;
+  for (var i = 0; i < num.length; i++) {
+    sumatoria = sumatoria + num[i] * 2**(num.length-1-i);
+  }
+  return sumatoria;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var binario = '';
+  while (num>=1) {
+    binario = num%2 + binario;
+    num = Math.floor(num/2);
+  }
+  return binario;
 }
 
 
